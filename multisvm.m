de0bc839679904789864bc67e3fc716f9ebc8e44
Shashankrd <sdeshpa1@andrew.cmu.edu>
2014-11-01 16:39:08 -0700

    Create multisvm.m

---
+++ multisvm.m
@@ -0,0 +1,17 @@
+function [result] = multisvm(TrainingSet,GroupTrain,TestSet)
+
+u=unique(GroupTrain);
+numClasses=length(u);
+result = zeros(length(TestSet(:,1)),1);
+for k=1:numClasses
+    G1vAll=(GroupTrain==u(k));
+    models(k) = svmtrain(TrainingSet,G1vAll,'BoxConstraint',0.5, 'kktviolationlevel',1);
+end
+for j=1:size(TestSet,1)
+    for k=1:numClasses
+        if(svmclassify(models(k),TestSet(j,:))) 
+            break;
+        end
+    end
+    result(j) = k;
+end
