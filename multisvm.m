2684a7f2056bc6be1972307703a680f2d28f02d7
Shashankrd <sdeshpa1@andrew.cmu.edu>
2014-11-02 03:16:46 -0800

    Update multisvm.m
    parameters tuned

--- multisvm.m
+++ multisvm.m
@@ -5,7 +5,7 @@ numClasses=length(u);
 result = zeros(length(TestSet(:,1)),1);
 for k=1:numClasses
     G1vAll=(GroupTrain==u(k));
-    models(k) = svmtrain(TrainingSet,G1vAll,'BoxConstraint',0.5, 'kktviolationlevel',1);
+    models(k) = svmtrain(TrainingSet,G1vAll,'kernel_function','rbf','BoxConstraint',0.5);
 end
 for j=1:size(TestSet,1)
     for k=1:numClasses
