325d5fc76721117a0a2b14b33d196d53db259480
Fmelle <fridtjof.melle@gmail.com>
2014-11-04 01:31:59 -0500

    Added conf matrix visualization

--- baseline_knn.m
+++ baseline_knn.m
@@ -19,6 +19,9 @@ optimalK = kNNValidateK(features_validation, ...
 voteKNN = kNearestNeighbor(features_train, ... 
     labels_train, features_test, labels_test, optimalK);
 
+labels = '32 49 50 51 52 53 54 55 56 57 58';
+printmat(nConfkNN, 'Confusion matrix', labels, labels)
+
 end
 
 
